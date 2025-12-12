ae3c91c19657053254f070fb28081b64cc64db8f
Gl1tchx <Gl1tchx4042@outlook.com>
2025-12-12 13:36:58 +0000

    Update GlitchedTools_raider.py

--- GlitchedTools_raider.py
+++ GlitchedTools_raider.py
@@ -164,21 +164,14 @@ class DiscordRaider:
 
     def display_logo(self):
         logo = f"""
-{PURPLE}██████╗ {PURPLE} ██╗ {PURPLE}     ██╗{PURPLE}████████╗ {PURPLE}██████╗{PURPLE}██╗  ██╗{PURPLE}███████╗{PURPLE}██████╗ {PURPLE}
-██╔════╝ {PURPLE}██║ {PURPLE}     ██║{PURPLE}╚══██╔══╝{PURPLE}██╔════╝{PURPLE}██║  ██║{PURPLE}██╔════╝{PURPLE}██╔══██╗{PURPLE}
-██║  ███╗{PURPLE}██║ {PURPLE}     ██║{PURPLE}   ██║   {PURPLE}██║     {PURPLE}███████║{PURPLE}█████╗  {PURPLE}██║  ██║{PURPLE}
-██║   ██║{PURPLE}██║ {PURPLE}     ██║{PURPLE}   ██║   {PURPLE}██║     {PURPLE}██╔══██║{PURPLE}██╔══╝  {PURPLE}██║  ██║{PURPLE}
-╚██████╔╝{PURPLE}███████╗{PURPLE} ██║{PURPLE}   ██║   {PURPLE}╚██████╗{PURPLE}██║  ██║{PURPLE}███████╗{PURPLE}██████╔╝{PURPLE}
- ╚═════╝ {PURPLE}╚══════╝{PURPLE} ╚═╝{PURPLE}   ╚═╝    {PURPLE}╚═════╝{PURPLE}╚═╝  ╚═╝{PURPLE}╚══════╝{PURPLE}╚═════╝ {PURPLE}
-                                                            
-████████╗ {PURPLE}██████╗  {PURPLE}██████╗ {PURPLE}██╗     {PURPLE}███████╗{PURPLE}                  
-╚══██╔══╝{PURPLE}██╔═══██╗{PURPLE}██╔═══██╗{PURPLE}██║     {PURPLE}██╔════╝{PURPLE}                  
-   ██║   {PURPLE}██║   ██║{PURPLE}██║   ██║{PURPLE}██║     {PURPLE}███████╗{PURPLE}                  
-   ██║   {PURPLE}██║   ██║{PURPLE}██║   ██║{PURPLE}██║     {PURPLE}╚════██║{PURPLE}                  
-   ██║   {PURPLE}╚██████╔╝{PURPLE}╚██████╔╝{PURPLE}███████╗{PURPLE}███████║{PURPLE}                  
-   ╚═╝    {PURPLE}╚═════╝  {PURPLE}╚═════╝ {PURPLE}╚══════╝{PURPLE}╚══════╝{PURPLE}                  
-         
-
+        {PURPLE}
+  ________.__  ____  __         .__           ___________           .__          
+ /  _____/|  |/_   |/  |_  ____ |  |__ ___  __\__    ___/___   ____ |  |   ______
+/   \  ___|  | |   \   __\/ ___\|  |  \\  \/  / |    | /  _ \ /  _ \|  |  /  ___/
+\    \_\  \  |_|   ||  | \  \___|   Y  \>    <  |    |(  <_> |  <_> )  |__\___ \ 
+ \______  /____/___||__|  \___  >___|  /__/\_ \ |____| \____/ \____/|____/____  >
+        \/                    \/     \/      \/                               \/          
+         {PURPLE}               
 
 {PURPLE}Discord Raider | Made by: Glitched Tools{RESET}
 """
